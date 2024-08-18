b4e0a6fc69f098966c5b2d1be3456747f3cb6376
Luis Torres <torrescereno@gmail.com>
2024-08-18 12:33:34 -0400

    add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,5 +9,9 @@ export default defineConfig({
   server: {
     port: 4000,
   },
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
 
