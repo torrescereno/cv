2323664fed3ae1703405a9a4c0f93b9fe1d4e279
Luis Torres <torrescereno@gmail.com>
2024-08-21 00:49:01 -0400

    remove analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   server: {
     port: 4000,
   },
-  output: "server",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
 });
